80d5aab0d0d508a3d8eff40606fc21479dfb04d9
Mikhail Bondarevsky <mibon@microsoft.com>
2021-02-24 22:14:14 +0000

    Change Description to DescriptionLabelCard

--- Source/Microsoft.Teams.Apps.EmployeeTraining/ClientApp/src/components/common/date-picker/datepicker.tsx
+++ Source/Microsoft.Teams.Apps.EmployeeTraining/ClientApp/src/components/common/date-picker/datepicker.tsx
@@ -30,10 +30,7 @@ let dayPickerStrings = {
     months: ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"],
     shortMonths: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
     days: ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"],
-    shortDays: ["S", "M", "T", "W", "T", "F", "S"],
-    prevMonthAriaLabel: "Previous month",
-    nextMonthAriaLabel: "Next month",
-    goToToday: ""
+    shortDays: ["S", "M", "T", "W", "T", "F", "S"]
 };
 
 const onFormatDate = (date?: Date): string => {
